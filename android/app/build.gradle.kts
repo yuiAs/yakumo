4c058a21358d4f79f4138989f08e78d67d8bb68b
yuinshielAs <u5h10n04@s1nc3r34dv1c3.com>
2026-06-09 16:45:55 +0900

    chore: set app versionName to 0.1.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0"
         buildConfigField("String", "GIT_HASH", "\"${gitHash()}\"")
     }
 
