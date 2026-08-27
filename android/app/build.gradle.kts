9b25b8f4f48afc3489071cb6b860aa90e4005b86
yuinshielAs <u5h10n04@s1nc3r34dv1c3.com>
2026-08-27 20:19:45 +0900

    chore: bump app version to 0.4.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -51,8 +51,8 @@ android {
         applicationId = "app.rly3h.yakumo"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.4.0"
         buildConfigField("String", "GIT_HASH", "\"${gitHash()}\"")
     }
 
